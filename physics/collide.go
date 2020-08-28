a568735fba3f0a43ecef4e1cc53f44b3daa946da
Ruslan Abelharisov <abelharisov@gmail.com>
2020-08-28 18:57:59 +0300

    physics

--- physics/collide.go
+++ physics/collide.go
@@ -5,16 +5,39 @@ import (
 )
 
 func (p *Physics) initCollisionsMap() {
+	tank := map[i.ObjectType]bool{
+		i.ObjectTypePlayerTank1: true,
+		i.ObjectTypePlayerTank2: true,
+		i.ObjectTypePlayerTank3: true,
+		i.ObjectTypePlayerTank4: true,
+		i.ObjectTypeEnemyTank1:  true,
+		i.ObjectTypeEnemyTank2:  true,
+		i.ObjectTypeEnemyTank3:  true,
+		i.ObjectTypeEnemyTank4:  true,
+		i.ObjectTypeBrickWall:   true,
+		i.ObjectTypeIronWall:    true,
+		i.ObjectTypeWater:       true,
+		i.ObjectTypeProjectile:  true,
+	}
+
 	p.collisionRules = map[i.ObjectType]map[i.ObjectType]bool{
-		i.ObjectTypePlayerTank1: {
-			i.ObjectTypePlayerTank1: true,
-			i.ObjectTypeBrickWall:   true,
-			i.ObjectTypeIronWall:    true,
-			i.ObjectTypeWater:       true,
-			i.ObjectTypeProjectile:  true,
-		},
+		i.ObjectTypePlayerTank1: tank,
+		i.ObjectTypePlayerTank2: tank,
+		i.ObjectTypePlayerTank3: tank,
+		i.ObjectTypePlayerTank4: tank,
+		i.ObjectTypeEnemyTank1:  tank,
+		i.ObjectTypeEnemyTank2:  tank,
+		i.ObjectTypeEnemyTank3:  tank,
+		i.ObjectTypeEnemyTank4:  tank,
 		i.ObjectTypeProjectile: {
 			i.ObjectTypePlayerTank1:  true,
+			i.ObjectTypePlayerTank2:  true,
+			i.ObjectTypePlayerTank3:  true,
+			i.ObjectTypePlayerTank4:  true,
+			i.ObjectTypeEnemyTank1:   true,
+			i.ObjectTypeEnemyTank2:   true,
+			i.ObjectTypeEnemyTank3:   true,
+			i.ObjectTypeEnemyTank4:   true,
 			i.ObjectTypeBrickWall:    true,
 			i.ObjectTypeIronWall:     true,
 			i.ObjectTypeHeadquarters: true,
