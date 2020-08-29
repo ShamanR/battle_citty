ee28d13df72ed72dcbaa5dcd65a94e9efc89eda9
Ruslan Abelharisov <abelharisov@gmail.com>
2020-08-29 12:37:06 +0300

    fix damage from own bullet

--- scene/objects/tank/tank.go
+++ scene/objects/tank/tank.go
@@ -8,7 +8,6 @@ import (
 	"github.com/shamanr/battle_citty/scene/objects/projectile"
 )
 
-
 // NewTank возвращает объект танка
 func NewTank(obj *object.MovableObject, bullet *projectile.Projectile) *Tank {
 	t := &Tank{
@@ -26,7 +25,7 @@ type Tank struct {
 }
 
 func (t *Tank) Shoot() {
-	startPos := pixel.V(t.GetPos().X + t.GetSize().Max.X, t.GetPos().Y + t.GetSize().Max.Y)
+	startPos := pixel.V(t.GetPos().X+t.GetSize().Max.X, t.GetPos().Y+t.GetSize().Max.Y)
 	t.Bullet.SetPos(&startPos)
 	t.AddChild(t.Bullet)
 	switch t.GetOrientation() {
@@ -46,6 +45,17 @@ func (t *Tank) Shoot() {
 }
 
 func (t *Tank) OnDamage(other interfaces.SceneObject) {
+	if other.GetObjectType() != consts.ObjectTypeProjectile {
+		return
+	}
+
+	childs := t.GetObjects()
+	for _, ch := range childs {
+		if ch.GetID() == other.GetID() {
+			return
+		}
+	}
+
 	t.SceneObject.SetLife(t.SceneObject.GetLife() - 1)
 	if t.SceneObject.GetLife() <= 0 {
 		t.SceneObject.GetScene().GetSpawner().Spawn(consts.ObjectTypeExplosion, *t.SceneObject.GetPos())
